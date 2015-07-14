b0b58c24d26ec13938b5dd38fb7e86f09d07dd9c
lanak1301 <musiconstellation@gmail.com>
2015-07-14 15:00:34 -0400

    day 2

--- script.js
+++ script.js
@@ -1,134 +1,216 @@
-var blackJack = {
-  deck : [
-    { value: 1 , image: "img/card/spadesAce.png" },
-    { value: 2 , image: "img/card/spades2.png" },
-    { value: 3 , image: "img/card/spades3.png" },
-    { value: 4 , image: "img/card/spades4.png" },
-    { value: 5 , image: "img/card/spades5.png" },
-    { value: 6 , image: "img/card/spades6.png" },
-    { value: 7 , image: "img/card/spades7.png" },
-    { value: 8 , image: "img/card/spades8.png" },
-    { value: 9 , image: "img/card/spades9.png" },
-    { value: 10 , image: "img/card/spades10.png" },
-    { value: 10 , image: "img/card/spadesJack.png" },
-    { value: 10 , image: "img/card/spadesQueen.png" },
-    { value: 10 , image: "img/card/spadesKing.png" },
+// $(function() {
+    var blackJack = {
+      deck : [
+        { value: 1 , image: "img/card/spadesAce.png" },
+        { value: 2 , image: "img/card/spades2.png" },
+        { value: 3 , image: "img/card/spades3.png" },
+        { value: 4 , image: "img/card/spades4.png" },
+        { value: 5 , image: "img/card/spades5.png" },
+        { value: 6 , image: "img/card/spades6.png" },
+        { value: 7 , image: "img/card/spades7.png" },
+        { value: 8 , image: "img/card/spades8.png" },
+        { value: 9 , image: "img/card/spades9.png" },
+        { value: 10 , image: "img/card/spades10.png" },
+        { value: 10 , image: "img/card/spadesJack.png" },
+        { value: 10 , image: "img/card/spadesQueen.png" },
+        { value: 10 , image: "img/card/spadesKing.png" },
 
-    { value: 1 , image: "img/card/heartsAce.png" },
-    { value: 2 , image: "img/card/hearts2.png" },
-    { value: 3 , image: "img/card/hearts3.png" },
-    { value: 4 , image: "img/card/hearts4.png" },
-    { value: 5 , image: "img/card/hearts5.png" },
-    { value: 6 , image: "img/card/hearts6.png" },
-    { value: 7 , image: "img/card/hearts7.png" },
-    { value: 8 , image: "img/card/hearts8.png" },
-    { value: 9 , image: "img/card/hearts9.png" },
-    { value: 10 , image: "img/card/hearts10.png" },
-    { value: 10 , image: "img/card/heartsJack.png" },
-    { value: 10 , image: "img/card/heartsQueen.png" },
-    { value: 10 , image: "img/card/heartsKing.png" },
+        { value: 1 , image: "img/card/heartsAce.png" },
+        { value: 2 , image: "img/card/hearts2.png" },
+        { value: 3 , image: "img/card/hearts3.png" },
+        { value: 4 , image: "img/card/hearts4.png" },
+        { value: 5 , image: "img/card/hearts5.png" },
+        { value: 6 , image: "img/card/hearts6.png" },
+        { value: 7 , image: "img/card/hearts7.png" },
+        { value: 8 , image: "img/card/hearts8.png" },
+        { value: 9 , image: "img/card/hearts9.png" },
+        { value: 10 , image: "img/card/hearts10.png" },
+        { value: 10 , image: "img/card/heartsJack.png" },
+        { value: 10 , image: "img/card/heartsQueen.png" },
+        { value: 10 , image: "img/card/heartsKing.png" },
 
-    { value: 1 , image: "img/card/clubsAce.png" },
-    { value: 2 , image: "img/card/clubs2.png" },
-    { value: 3 , image: "img/card/clubs3.png" },
-    { value: 4 , image: "img/card/clubs4.png" },
-    { value: 5 , image: "img/card/clubs5.png" },
-    { value: 6 , image: "img/card/clubs6.png" },
-    { value: 7 , image: "img/card/clubs7.png" },
-    { value: 8 , image: "img/card/clubs8.png" },
-    { value: 9 , image: "img/card/clubs9.png" },
-    { value: 10 , image: "img/card/clubs10.png" },
-    { value: 10 , image: "img/card/clubsJack.png" },
-    { value: 10 , image: "img/card/clubsQueen.png" },
-    { value: 10 , image: "img/card/clubsKing.png" },
+        { value: 1 , image: "img/card/clubsAce.png" },
+        { value: 2 , image: "img/card/clubs2.png" },
+        { value: 3 , image: "img/card/clubs3.png" },
+        { value: 4 , image: "img/card/clubs4.png" },
+        { value: 5 , image: "img/card/clubs5.png" },
+        { value: 6 , image: "img/card/clubs6.png" },
+        { value: 7 , image: "img/card/clubs7.png" },
+        { value: 8 , image: "img/card/clubs8.png" },
+        { value: 9 , image: "img/card/clubs9.png" },
+        { value: 10 , image: "img/card/clubs10.png" },
+        { value: 10 , image: "img/card/clubsJack.png" },
+        { value: 10 , image: "img/card/clubsQueen.png" },
+        { value: 10 , image: "img/card/clubsKing.png" },
 
-    { value: 1 , image: "img/card/diamondsAce.png" },
-    { value: 2 , image: "img/card/diamonds2.png" },
-    { value: 3 , image: "img/card/diamonds3.png" },
-    { value: 4 , image: "img/card/diamonds4.png" },
-    { value: 5 , image: "img/card/diamonds5.png" },
-    { value: 6 , image: "img/card/diamonds6.png" },
-    { value: 7 , image: "img/card/diamonds7.png" },
-    { value: 8 , image: "img/card/diamonds8.png" },
-    { value: 9 , image: "img/card/diamonds9.png" },
-    { value: 10 , image: "img/card/diamonds10.png" },
-    { value: 10 , image: "img/card/diamondsJack.png" },
-    { value: 10 , image: "img/card/diamondsQueen.png" },
-    { value: 10 , image: "img/card/diamondsKing.png" }
-        ] ,
+        { value: 1 , image: "img/card/diamondsAce.png" },
+        { value: 2 , image: "img/card/diamonds2.png" },
+        { value: 3 , image: "img/card/diamonds3.png" },
+        { value: 4 , image: "img/card/diamonds4.png" },
+        { value: 5 , image: "img/card/diamonds5.png" },
+        { value: 6 , image: "img/card/diamonds6.png" },
+        { value: 7 , image: "img/card/diamonds7.png" },
+        { value: 8 , image: "img/card/diamonds8.png" },
+        { value: 9 , image: "img/card/diamonds9.png" },
+        { value: 10 , image: "img/card/diamonds10.png" },
+        { value: 10 , image: "img/card/diamondsJack.png" },
+        { value: 10 , image: "img/card/diamondsQueen.png" },
+        { value: 10 , image: "img/card/diamondsKing.png" }
+            ] ,
 
-  player : {
-    cardsLocation: $("div.player"),
-    cards : [] ,
-  } ,
+      player : {
+        $cardsLocation: $("div.player"),
+        $scoreLocation: $("#playerScore"),
+        score : 0,
+        cards : [] ,
+      } ,
 
-  dealer : {
-    cardsLocation: $("div.dealer"),
-    cards : [] ,
-  } ,
+      dealer : {
+        $cardsLocation: $("div.dealer"),
+        $scoreLocation: $("#dealerScore"),
+        score : 0,
+        cards : [] ,
+      } ,
 
-  dealCard : function(person, facedown) {
-    var cardIndex = Math.floor(Math.random() * this.deck.length) ;
-    if (facedown) {
-          $("<img />").appendTo(person.cardsLocation).attr( "src", "img/card/back.png")
-                      .css({marginLeft: '150px', opacity: '0'}).animate({marginLeft: '0px', opacity: '1'});
-    } else {
-          $("<img />").appendTo(person.cardsLocation).attr( "src", this.deck[cardIndex].image)
-                      .css({marginLeft: '150px', opacity: '0'}).animate({marginLeft: '0px', opacity: '1'});
-    }
-    var card = this.deck.splice(cardIndex, 1)[0]
-    person.cards.push(card);
-  } ,
-
-  updateScore : function(person) {
-    var lowScore = 0;
-    var hasAce = false;
-      for (var i=0; i < person.cards.length; i++) {
-        hasAce |= person.cards[i].value === 1;  // if hasAce has been already set to true it'll stay true, otherwise it's equal to value === 1
-        lowScore += person.cards[i].value ;
-      }
-
-      $("#playerScore").text(lowScore)
-      if (hasAce) {
-        highScore = lowScore + 10;
-        if (highScore < 21) {
-          $("#playerScore").text( lowScore + "|" + highScore )
-        } else if (highScore === 21) {
-          $("#playerScore").text( highScore )
+      dealCard : function(person, facedown, callback) {
+        var cardIndex = Math.floor(Math.random() * this.deck.length) ;
+        if (facedown) {
+              $("<img />").appendTo(person.$cardsLocation).attr( "src", "img/card/back.png")
+                          .css({ marginLeft: '500px', opacity: '0'}).animate({ marginLeft: '-40px', opacity: '1'}, 400, callback);
+        } else {
+              $("<img />").appendTo(person.$cardsLocation).attr( "src", this.deck[cardIndex].image)
+                          .css({ marginLeft: '540px', opacity: '0'}).animate({ marginLeft: '-40px', opacity: '1'}, 400, callback);
         }
+        var card = this.deck.splice(cardIndex, 1)[0]
+        person.cards.push(card);
+      } ,
+
+      updateScore : function(person) {
+        var lowScore = 0;
+        var hasAce = false;
+          for (var i=0; i < person.cards.length; i++) {
+            hasAce |= person.cards[i].value === 1;  // if hasAce has been already set to true it'll stay true, otherwise it's equal to value === 1
+            lowScore += person.cards[i].value ;
+          }
+
+          person.$scoreLocation.text(lowScore)
+          person.score = lowScore;
+          if (hasAce) {
+            highScore = lowScore + 10;
+            if (highScore < 21) {
+              person.$scoreLocation.text( lowScore + "|" + highScore )
+              person.score = highScore;
+            } else if (highScore === 21) {
+              person.$scoreLocation.text( highScore )
+              person.score = highScore;
+            }
+          }
+      } ,
+
+      setUpHitBtn : function() {
+        var self = this;
+        var $button = $(":button#hit");
+        $button.on( "click", function() {
+            self.dealCard(self.player);
+            self.updateScore(self.player);
+            self.checkIfRoundIsOver(self.player);
+        })
+      } ,
+
+      setUpStandBtn : function() {
+        var self = this;
+        var $button = $(":button#stand");
+        $button.on( "click", function() {
+            self.endHand();
+        })
+      } ,
+
+      checkIfRoundIsOver : function(person) {
+        var self = this;
+        if ( person.score === 21 || person.score > 21 ) {
+          setTimeout(function () {
+              self.endHand();
+          }, 500);
+        }
+      } ,
+
+      endHand : function () {
+        $(":button#hit").attr("disabled","disabled");
+        $(":button#stand").attr("disabled","disabled");
+        $("div.dealer > img:nth-of-type(2)").attr( "src", blackJack.dealer.cards[1].image);
+        this.dealerDrawCards();
+      },
+
+      dealerDrawCards : function() {
+        var self = blackJack;
+        self.updateScore(self.dealer);
+        var score = parseInt(self.dealer.score);
+        if (score < 17) {
+          self.dealCard(self.dealer, false, self.dealerDrawCards);
+        } else {
+          self.showFinalResults();
+        }
+      },
+
+      showFinalResults : function () {
+        var result;
+        var $header = $(".popup h2");
+        if (this.player.score > 21) { result = "Busted" }
+        else if (this.dealer.score > 21) {result = "Busted"}
+        else if (this.player.score === 21) {
+             if (this.dealer.score) { result = "Pushed"} result = "BlackJack" }
+        else if (this.dealer.score === 21 ||
+                 this.dealer.score > this.player.score ) { result = "You lost" }
+        else if ( this.player.score > this.dealer.score) { result = "You win" }
+
+        $header.text(result);
+        $(".dealerScore").text(this.dealer.score);
+        $(".playerScore").text(this.player.score);
+        $(".overlay").addClass("overplay");
+        this.resetGame();
+      },
+
+      resetGame : function() {
+        var self = this;
+        var $playAgain = $("a.close");
+        $playAgain.on( "click", function() {
+           var array = $.merge( self.player.cards.splice(0, self.player.cards.length), self.dealer.cards.splice(0, self.dealer.cards.length));
+          $.merge( self.deck, array);
+        $(".overlay").removeClass("overplay");
+        $(".dealer").html(null)
+        $(".player").html(null)
+        $("#dealerScore").html(null)
+        setTimeout(function () {
+          self.dealHand()
+        }, 500);
+        })
+      } ,
+
+      dealHand : function() {
+        var self = this;
+        this.dealCard(this.player) ;
+
+        var timeout = setTimeout(function () {
+          self.dealCard(self.dealer)
+        }, 500);
+
+        var timeout = setTimeout(function () {
+          self.dealCard(self.player)
+          self.updateScore(self.player)
+        }, 1000);
+
+        var timeout = setTimeout(function () {
+          self.dealCard(self.dealer, true)
+          self.checkIfRoundIsOver(self.player);
+          self.setUpHitBtn()
+          self.setUpStandBtn()
+        }, 1500);
       }
-  } ,
+      //end of blackJack object
+    }
 
-  setUpHitBtn : function() {
-    var self = this;
-    $button = $(":button#hit");
-    $button.on( "click", function() {
-      self.dealCard(self.player);
-      self.updateScore(self.player)
-    })
-  } ,
+    blackJack.dealHand() ;
+// });
 
-  setUpStandBtn : function() {
-    //
-  } ,
-
-  dealHand : function() {
-    var self = this;
-    this.dealCard(this.player)
-    var timeout = setTimeout(function () {
-      self.dealCard(self.dealer)
-    }, 500);
-    var timeout = setTimeout(function () {
-      self.dealCard(self.player)
-      self.updateScore(self.player)
-    }, 1000)
-    var timeout = setTimeout(function () {
-      self.dealCard(self.dealer, true)
-    }, 1500);
-    this.setUpHitBtn()
-  }
-  //end of blackJack object
-}
-
-
-blackJack.dealHand() ;
+//create overplay for Play Game
+//change possible outcomes conditional statements
